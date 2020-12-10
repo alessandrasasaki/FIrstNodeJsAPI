9a96f9e75f4359660d16efc580efbf7bf0449d28
Alessandra Sasaki <ale.sasaki@hotmail.com>
2020-12-10 08:58:06 -0300

    created GET route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const index = require("./routes/index");
 
 app.use(express.json());
+app.use("/", index);
 
 app.use((req, res, next)=> {
   res.header("Access-Control-Allow-Origin", "*");
